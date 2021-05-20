aac827649b795ea2ff346f92b20866bca3770b70
MuhammadNizamani <49721249+MuhammadNizamani@users.noreply.github.com>
2021-05-20 12:28:19 +0500

    image preview is added

--- lib/screens/edit_products_screen.dart
+++ lib/screens/edit_products_screen.dart
@@ -9,18 +9,24 @@ class EditProductsScreen extends StatefulWidget {
 class _EditProductsScreenState extends State<EditProductsScreen> {
   final _priceFocusNode = FocusNode();
   final _descriptionFocusNode = FocusNode();
+  final _imageUrlControler = TextEditingController();
   @override
   void dispose() {
     _priceFocusNode.dispose();
     _descriptionFocusNode.dispose();
+    _imageUrlControler.dispose();
     super.dispose();
   }
 
+  void _saveForm() {}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Edit Produt"),
+        actions: [
+          IconButton(icon: Icon(Icons.save), onPressed: _saveForm),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
@@ -49,6 +55,34 @@ class _EditProductsScreenState extends State<EditProductsScreen> {
                 keyboardType: TextInputType.multiline,
                 focusNode: _descriptionFocusNode,
               ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  Container(
+                      width: 100,
+                      height: 100,
+                      margin: EdgeInsets.only(top: 8, right: 10),
+                      decoration: BoxDecoration(
+                          border: Border.all(width: 1, color: Colors.grey)),
+                      child: _imageUrlControler.text.isEmpty
+                          ? Text("Enter a URL ")
+                          : FittedBox(
+                              child: Image.network(_imageUrlControler.text,
+                                  fit: BoxFit.cover),
+                            )),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'Image Url'),
+                      keyboardType: TextInputType.url,
+                      textInputAction: TextInputAction.done,
+                      controller: _imageUrlControler,
+                      onFieldSubmitted: (_) {
+                        _saveForm();
+                      },
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
         ),
